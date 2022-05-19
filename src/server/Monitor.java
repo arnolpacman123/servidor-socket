d5e1d2dc7b47eb359c8ccbb9b7672e7bcac5c6fa
Arnol Guevara <arnolpacman@gmail.com>
2022-05-19 10:01:26 -0400

    commit

--- src/server/Monitor.java
+++ src/server/Monitor.java
@@ -17,30 +17,6 @@ public class Monitor implements SocketListenerI {
         int port = getPortProperty(args);
         Server server = new Server(port);
         server.start();
-//        try {
-//            InetAddress ip = InetAddress.getByName("localhost");
-//            Socket socket = new Socket(ip, 5000);
-//            while (true) {
-//                Scanner reader = new Scanner(System.in);
-//                System.out.println("Escribe el destinatario:");
-//                String id = reader.nextLine();
-//                System.out.println("Escribe la temperatura:");
-//                int temperature = Integer.parseInt(reader.nextLine());
-//                System.out.println("Escribe la humedad:");
-//                int humidity = Integer.parseInt(reader.nextLine());
-//                OutputStream outputStream = socket.getOutputStream();
-//                ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
-//                objectOutputStream.writeObject(new DataClient(id, temperature, humidity));
-//                InputStream inputStream = socket.getInputStream();
-//                ObjectInputStream objectInputStream = new ObjectInputStream(inputStream);
-//                Data data = (Data) objectInputStream.readObject();
-//                System.out.println("EL SERVIDOR RESPONDE:");
-//                System.out.println(data.getTemperature());
-//                System.out.println(data.getHumidity());
-//            }
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
     }
 
     public static int getPortProperty(String[] args) {
